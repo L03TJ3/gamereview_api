8c810f8e487331ae90745f8e01e9d78159b06086
Laurence <laurenceweening@gmail.com>
2016-04-29 16:55:05 +0200

    uncomment mount_uploader

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :category
   has_many :reviews
   validates_presence_of :title, :category
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
 
 end
